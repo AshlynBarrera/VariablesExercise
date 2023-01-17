4544ea59c43dd0f86eb7e5f83e9d866963f34769
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-17 10:02:20 -0600

    modifications made to exercise

--- script.js
+++ script.js
@@ -3,3 +3,49 @@
 
 // 2
 let num = 20
+console.log(num);
+
+//3 
+num = 15
+console.log(num);
+
+//4
+//num += 7;
+num = num + 7;
+console.log(num);
+
+
+//5
+num --;
+console.log(num);
+
+
+//6
+num = "This is not a string"
+console.log(num);
+
+//7
+let blank;
+
+
+//8
+console.log(blank); // undefined value
+
+
+//9
+blank = null;
+console.log(blank); // null value 
+
+//BONUS 
+// 1
+const quarter = 0.25, dime = 0.10,nickle = 0.05, penny = 0.01;
+
+//2 
+console.log(quarter,dime,nickle,penny);
+
+//3 
+ let letters = "qwertyuiop"
+
+
+//4 
+console.log(letters[letters.length - 1]);
